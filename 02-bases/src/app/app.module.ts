f24d2846490494a21bced924032d10738727b8c4
Mauricio Aviles <mauavilespi@outlook.com>
2023-09-01 16:53:05 -0600

    Ejercicios Sección 5: Expandir Bases de Angular

--- 02-bases/src/app/app.module.ts
+++ 02-bases/src/app/app.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { CounterModule } from './counter/counter.module';
-import { AppComponent } from './app.component';
+import { DbzModule } from './dbz/dbz.module';
 import { HeroesModule } from './heroes/heroes.module';
+
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
@@ -12,6 +14,7 @@ import { HeroesModule } from './heroes/heroes.module';
   imports: [
     BrowserModule,
     CounterModule,
+    DbzModule,
     HeroesModule
   ],
   providers: [],
